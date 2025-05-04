180cba59f64fe4e030918a6de7a598d59afd6d54
yatharthrathii <yatharthmaheshwari01@gmail.com>
2025-05-04 12:57:48 +0530

    summmary

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import MealsSummary from './components/MealsSummary'
 
 function App() {
   return (
     <>
       <Header />
+      <MealsSummary />
     </>
   )
 }
