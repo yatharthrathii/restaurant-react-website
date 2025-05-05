b426c3d83b9729d2ebb62276d470108c8ecd2813
yatharthrathii <yatharthmaheshwari01@gmail.com>
2025-05-05 14:59:56 +0530

    AvailableMeals-Dummydata

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import MealsSummary from './components/MealsSummary'
+import AvailableMeals from './components/AvailableMeals';
 
 function App() {
   return (
     <>
       <Header />
       <MealsSummary />
+      <AvailableMeals />
     </>
   )
 }
---
+++ src/components/AvailableMeals.jsx
@@ -0,0 +1,48 @@
+import './AvailableMeals.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  return (
+    <section className="meals">
+      <ul>
+        {DUMMY_MEALS.map((meal) => (
+          <li key={meal.id} className="mealItem">
+            <div>
+              <h3>{meal.name}</h3>
+              <p className="description">{meal.description}</p>
+              <span className="price">${meal.price.toFixed(2)}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
